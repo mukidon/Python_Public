657073e1a55b116de707f3f236809e6d6526dbd2
Mukesh B <www.muki1999@gmail.com>
2022-04-15 22:58:59 +0530

    fourth commit

--- imageCompression.py
+++ imageCompression.py
@@ -3,9 +3,17 @@ import os
 import PIL
 import glob
 
-image = Image.open('../images/small-39941552906266.jpg')
-print(image.size)
+ListOfFiles=os.listdir('../images')
+for x in ListOfFiles:
+    image = Image.open("../images/"+x)
+    print(x)
+    resized_image = image.resize((219,219))
+    resized_image = resized_image.convert("RGB")
+    resized_image.save('doctors_testing/'+x)
+        
+# image = Image.open('../images/small-39941552906266.jpg')
+# print(image.size)
 
-resized_image = image.resize((219,219))
-resized_image.save('resizedImage.jpg')
-print(resized_image.size)
+# resized_image = image.resize((219,219))
+# resized_image.save('resizedImage.jpg')
+# print(resized_image.size)
